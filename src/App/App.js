cc8c867e48e09c710ff8d1803ca0a5b0b3e16a09
Steve <stevestephenas7@gmail.com>
2020-11-27 04:30:41 +0100

    Final changes for frontend

--- src/App/App.js
+++ src/App/App.js
@@ -7,6 +7,15 @@ import CheckOutPage from "../CheckOutPage/CheckOutPage";
 import LoginPage from "../LoginPage/LoginPage";
 import { useStateValue } from "../StateProvider";
 import { auth } from "../firebase";
+import Payment from "../PaymentPage/Payment";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import Orders from "../OrdersPage/Orders";
+import Footer from "../FooterPage/Footer";
+
+const promise = loadStripe(
+  "pk_test_51HqCVFFmH5npfTZWvSEkkBBSVjNXePGMdFKFCOUBkh2PmqV0bbnc0nB40dCObHNTGAg615hIvD1OQKcKYCBY3OPs00usEKfOBm"
+);
 
 function App() {
   const [{ user }, dispatch] = useStateValue();
@@ -32,23 +41,41 @@ function App() {
     };
   }, []);
 
-  console.log("user is - ", user);
+  // console.log("user is - ", user);
+  const truncate = (email, n = 6) => {
+    const index = email?.indexOf("@");
+    return email?.substr(0, index);
+  };
 
   return (
     <Router>
       <div className="App">
         <Switch>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+            <Footer />
+          </Route>
           <Route path="/login">
             <LoginPage />
           </Route>
           <Route path="/checkout">
             <Header />
-            <CheckOutPage />
+            <CheckOutPage username={truncate(user?.email)} />
+            <Footer />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
+            <Footer />
           </Route>
           {/* This is default */}
           <Route path="/">
             <Header />
             <HomePage />
+            <Footer />
           </Route>
         </Switch>
       </div>
